dc396a3f5c877b868c98a08d1e3d3430752b1cf8
Brian Love <brian@brianflove.com>
2018-07-30 20:35:14 -0600

    Ngui Map

--- course/src/app/shared/components/resorts-map/resorts-map.component.ts
+++ course/src/app/shared/components/resorts-map/resorts-map.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import { Resort } from '@app/models/resort.model';
+import { NguiMapComponent } from '@ngui/map';
 
 interface Marker {
-  lat: number;
-  lng: number;
+  position: number[];
   title: string;
 }
 
@@ -12,13 +18,49 @@ interface Marker {
   templateUrl: './resorts-map.component.html',
   styleUrls: ['./resorts-map.component.scss']
 })
-export class ResortsMapComponent {
+export class ResortsMapComponent implements OnChanges {
   @Input() height: number;
   @Input() resorts: Resort[];
   @Input() selectedResort: Resort;
+  @Input() zoom: number;
 
-  lat = 39.8283;
-  lng = -98.5795;
+  bounds: google.maps.LatLngBounds;
+  map: google.maps.Map;
   markers: Marker[] = [];
-  zoom = 5;
+  @ViewChild(NguiMapComponent) nguiMapComponent: NguiMapComponent;
+  title: string;
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    if (simpleChanges.resorts && simpleChanges.resorts.currentValue) {
+      this.markers = this.resorts.map(resort => ({
+        position: [Number(resort.lat), Number(resort.lng)],
+        title: resort.name
+      }));
+    }
+  }
+
+  onMapClick() {
+    this.nguiMapComponent.closeInfoWindow('infoWindow');
+  }
+
+  onMapReady(map: google.maps.Map) {
+    this.map = map;
+    this.bounds = new google.maps.LatLngBounds();
+  }
+
+  onMarkerClick(event) {
+    const marker = event.target;
+    this.title = event.target.getTitle();
+    this.nguiMapComponent.openInfoWindow('infoWindow', marker);
+  }
+
+  onMarkerInit(marker) {
+    this.bounds.extend(marker.getPosition());
+    this.map.setCenter(this.bounds.getCenter());
+    if (this.resorts.length === 1) {
+      this.map.setZoom(this.zoom);
+    } else {
+      this.map.fitBounds(this.bounds);
+    }
+  }
 }
--- course/src/app/shared/shared.module.ts
+++ course/src/app/shared/shared.module.ts
@@ -1,13 +1,21 @@
-import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ResortsMapComponent } from '@app/shared/components/resorts-map/resorts-map.component';
+import { environment } from '@env/environment';
+import { NguiMapModule } from '@ngui/map';
 
 export const components = [ResortsMapComponent];
 
 @NgModule({
   declarations: components,
   exports: components,
-  imports: [AgmCoreModule, CommonModule]
+  imports: [
+    NguiMapModule.forRoot({
+      apiUrl: `https://maps.google.com/maps/api/js?key=${
+        environment.google.maps.apiKey
+      }`
+    }),
+    CommonModule
+  ]
 })
 export class SharedModule {}
